cab53b2e97ae7bed8b6cb19df9e61edacbbcb613
noeldevops <100243721+noeldevops@users.noreply.github.com>
2025-04-19 02:05:51 +0200

    Update NoeldevopsWebappApplication.java

--- noeldevops-webapp/src/main/java/com/noeldevops/demo/NoeldevopsWebappApplication.java
+++ noeldevops-webapp/src/main/java/com/noeldevops/demo/NoeldevopsWebappApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NoeldevopsWebappApplication {
+public class NoeldevopsWebappApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(NoeldevopsWebappApplication.class, args);
